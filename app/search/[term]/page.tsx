935e3c2eafa21cc8b8d10f7799edc8a74851e260
Nigel <nigeltakadza@gmail.com>
2023-07-06 21:16:34 +0200

    building the header component

---
+++ app/search/[term]/page.tsx
@@ -0,0 +1,11 @@
+ 'react'
+
+function page() {
+  return (
+    <div>
+        Welcmome to the search result page
+    </div>
+  )
+}
+
+export default page
---
+++ components/SearchButton.tsx
@@ -0,0 +1,16 @@
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+function SearchButton() {
+
+    const {pending} = useFormStatus();
+  return (
+   <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold
+   py-4 px-4 rounded-full disabled:opacity-50'>
+    {pending && "Searching..."}
+    {!pending && <MagnifyingGlassIcon className="h-5 w-5"/>}
+   </button>
+
+  );
+}
+
+export default SearchButton
